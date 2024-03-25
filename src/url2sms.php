2a7e2d45472b7a1f0133ebd14066cdb5bc285da4
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2024-03-25 17:48:56 +0100

    Json format forced for http

--- src/url2sms.php
+++ src/url2sms.php
@@ -2,6 +2,10 @@
 
 namespace SmsSend;
 
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+
 require_once '../vendor/autoload.php';
 
 $contact = $_GET['contact'];
